45760075ceddb017e26ac4cf75781c8d88b6f5e3
Nguyen9c1999 <nguyen9c1999@gmail.com>
2022-12-28 18:12:37 +0700

    show detail wallet in home

--- dist/src/service/wallet-service.d.ts
+++ dist/src/service/wallet-service.d.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
-declare class WalletService {
+export declare class WalletService {
     private walletRepository;
     constructor();
     findAll: () => Promise<any>;
     create: (wallet: any) => Promise<any>;
     delete: (req: Request, res: Response) => Promise<void>;
     edit: (req: Request, res: Response) => Promise<any>;
+    getWalletDetail: (idUser: any) => Promise<{
+        wallet: any;
+        transactions: any;
+    }>;
 }
 declare const _default: WalletService;
 export default _default;
