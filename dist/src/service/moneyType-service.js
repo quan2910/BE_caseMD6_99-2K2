05a9637b3be4c157142ae6d54311106a38c9a379
quan2910 <ngtruongquan@gmail.com>
2023-01-05 11:27:11 +0700

    git de pull

--- dist/src/service/moneyType-service.js
+++ dist/src/service/moneyType-service.js
@@ -1,1 +1,16 @@
-//# sourceMappingURL=moneyType-service.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MoneyTypeService = void 0;
+const data_source_1 = require("../data-source");
+const money_type_1 = require("../model/money-type");
+class MoneyTypeService {
+    constructor() {
+        this.getAll = async () => {
+            let moneyTypes = await this.moneyTypeRepo.find();
+            return moneyTypes;
+        };
+        this.moneyTypeRepo = data_source_1.AppDataSource.getRepository(money_type_1.MoneyType);
+    }
+}
+exports.MoneyTypeService = MoneyTypeService;
+//# sourceMappingURL=moneyType-Service.js.map
